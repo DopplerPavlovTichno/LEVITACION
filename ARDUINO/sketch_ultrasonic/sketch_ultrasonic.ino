aad69c22b1dc4dd243ba4082a85dd58271fdebec
jfdoppler <jdoppler@df.uba.ar>
2018-11-28 19:30:40 -0300

    Prueba del PWM con modulo y con transistor
    Con el módulo L298N no funcionó, no sabemos por qué. En un momento tiraba el voltaje correcto para alimentar al ventilador (alrededor de 10 V) pero no daba suficiente corriente, el ventilador no se movía.
    
    Con el transistor si funcionó. El circuito era un transistor BD139 con una R de base de alrededor de 5 kOhm, un voltaje de colector de alrededor de 10 V (este es el voltaje máximo, dado por una fuente de tensión continua que podemos elegir). La base está alimentada con el PWM (pin 9) a través de la resistencia de base. El emisor está a tierra.

--- ARDUINO/sketch_ultrasonic/sketch_ultrasonic.ino
+++ ARDUINO/sketch_ultrasonic/sketch_ultrasonic.ino
@@ -12,47 +12,64 @@ double error;
 double last_error;
 long duration;
 int distance;
-const int trigPin = 9;
-const int echoPin = 10;
+const int trigPin = 12;
+const int echoPin = 13;
 const int controlPin = 1;
+unsigned long delay1 = 100;
+unsigned long delay2 = 100;
+unsigned long t1 ;
+unsigned long t2 ;
+double pidresult ;
+int valor ;
+const int led = 9;
 
 void setup() {
   // initialize the serial port:
   pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
   pinMode(echoPin, INPUT); // Sets the echoPin as an Input
   Serial.begin(9600);
-  kp = 1.0;
-  ki = 0.0;
-  kd = 0.0;
-  setpoint = 300;
-  last_error = 0;
+  kp = 0.1;
+  ki = 0.01;
+  kd = 0.1;
+  setpoint = 250 ;
+  last_error = 0 ;
   dt = 1000;
+  t1 = t2 = millis() ;
 }
 
 void loop() {
-  // Clears the trigPin
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-   duration = pulseIn(echoPin, HIGH);
+  if(millis() - t1 > delay1) { //this will be executed every delay1 ms
+    valor = 200  ;
+    analogWrite(led,min(pidresult,255));
+    t1 = millis();
+  }
+  if(millis() - t2 > delay2) { //this will be executed every delay1 ms
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    // Sets the trigPin on HIGH state for 10 micro seconds
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
+    // Reads the echoPin, returns the sound wave travel time in microseconds
+    duration = pulseIn(echoPin, HIGH);
     // Calculating the distance
-  distance = duration*0.34/2;
-  Serial.println(distance);
-  calculatePID();
-  delay(dt);
+    distance = duration*0.34/2;
+    Serial.println(distance);
+    pidresult = calculatePID(distance, setpoint, kp, ki, kd);
+    t2 = millis() ;
+    //delay(dt);
+  }
 }
 
-void calculatePID() {
+double calculatePID(double distance, double setpoint, double kp, double ki, double kd) {
+  double PIDValue ;
   error = distance - setpoint;
   p_term = kp*error;
   i_term = i_term + ki*error*dt/1000;
   d_term = kd*(error-last_error)*1000/dt;;
   last_error = error;
   PIDValue = p_term + i_term + d_term;
- // Serial.println(PIDValue);
+  //Serial.println(PIDValue);
+  return PIDValue ;
  //analogWrite(controlPin, PIDValue);
 }
