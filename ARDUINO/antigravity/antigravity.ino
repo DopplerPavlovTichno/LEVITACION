7613e2d0133172e0cbf711ffe0bc65a15a7b7d4a
lucaspavlov <33254553+lucaspavlov@users.noreply.github.com>
2018-12-04 16:09:58 -0300

    Solucionado el setpoint, pero sin k's interactivos
    Basándome en el "Example 4" de http://forum.arduino.cc/index.php?topic=396450 pude poner el setpoint de una manera en la que se mantiene constante, pero tengo un solo número para poner como input; hay que modificar para agregar que la primera letra sea el keyword que diga si quiero modificar el setpoint o alguno de los k's (kp, ki, kd).
    
    Algo que noté: un delay de entre uno y cuatro segundos entre que mando el setpoint y efectivamente lo setea (imprime cada un segundo y hay veces que pasan hasta cuatro impresiones con el setpoint anterior).

--- ARDUINO/antigravity/antigravity.ino
+++ ARDUINO/antigravity/antigravity.ino
@@ -1,160 +1,53 @@
-// defines variables
-double kp;
-double ki;
-double kd;
-double p_term;
-double i_term;
-double d_term;
-double PIDValue;
-int setpoint;
-int dt;
-double error;
-double last_error;
-long duration;
-int distance;
-const int trigPin = 12;
-const int echoPin = 13;
-const int controlPin = 1;
-unsigned long delay1 = 100;
-unsigned long delay2 = 100;
-unsigned long t1 ;
-unsigned long t2 ;
-double pidresult ;
-double distancia_maxima ;
-double distancia_minima ;
-int valor ;
-const int led = 9;
-int para_actuador ;
+// Example 4 - Receive a number as text and convert it to an int
+
+const byte numChars = 32;
+char receivedChars[numChars];   // an array to store the received data
+
+boolean newData = false;
+
+int dataNumber = 0;             // new for this version
 
 void setup() {
-  // initialize the serial port:
-  pinMode(trigPin, OUTPUT); // Sets the trigPin as an Output
-  pinMode(echoPin, INPUT); // Sets the echoPin as an Input
-  Serial.begin(9600);
-  kp = 0.1;
-  ki = 0;
-  kd = 0;
-  setpoint = 250 ;
-  last_error = 0 ;
-  dt = 1000;
-  t1 = t2 = millis() ;
-  distancia_maxima = 400;
-  distancia_minima = 4;
+    Serial.begin(9600);
+    Serial.println("<Arduino is ready>");
 }
 
 void loop() {
-  if(millis() - t1 > delay1) { //this will be executed every delay1 ms
-    valor = 200  ;
-    para_actuador = calibracion(distance, 400, 4) ;
-    analogWrite(led,para_actuador);
-    t1 = millis();
-  }
-  if(millis() - t2 > delay2) { //this will be executed every delay1 ms
-    digitalWrite(trigPin, LOW);
-    delayMicroseconds(2);
-    // Sets the trigPin on HIGH state for 10 micro seconds
-    digitalWrite(trigPin, HIGH);
-    delayMicroseconds(10);
-    digitalWrite(trigPin, LOW);
-    // Reads the echoPin, returns the sound wave travel time in microseconds
-    duration = pulseIn(echoPin, HIGH);
-    // Calculating the distance
-    distance = duration*0.34/2;
-    Serial.println(distance);
-    pidresult = calculatePID(distance, setpoint, kp, ki, kd);
-    t2 = millis() ;
-    //delay(dt);
-  }
-    int i;
-    char val[4];
-    while(Serial.available()>0) {
-          val[i]=Serial.read();
-          i++;
+    recvWithEndMarker();
+    showNewNumber();
+}
+
+void recvWithEndMarker() {
+    static byte ndx = 0;
+    char endMarker = '\n';
+    char rc;
+    
+    if (Serial.available() > 0) {
+        rc = Serial.read();
+
+        if (rc != endMarker) {
+            receivedChars[ndx] = rc;
+            ndx++;
+            if (ndx >= numChars) {
+                ndx = numChars - 1;
+            }
+        }
+        else {
+            receivedChars[ndx] = '\0'; // terminate the string
+            ndx = 0;
+            newData = true;
+        }
     }
-    switch(val[0]){
-      case 'p':
-        kp=atoi(&val[1]);
-        Serial.print("Kp= ");
-        Serial.print(kp);
-        Serial.println();
-        break;
-      case 'i':
-        ki=atoi(&val[1]);
-        Serial.print("Ki= ");
-        Serial.print(ki);
-        Serial.println();
-        break;
-      case 'd':
-        kd=atoi(&val[1]);
-        Serial.print("Kd= ");
-        Serial.print(kd);
-        Serial.println();
-        break;
-      case 's':
-        setpoint=atoi(&val[1]);
-        Serial.print("setpoint= ");
-        Serial.print(setpoint);
-        Serial.println();
-        break;
-      default:
-        break;
-     }
-    
-    //potencia= atoi(&val[0]);
-    //}
-    Serial.print("Kp= ");
-    Serial.print(kp);
-    Serial.println();
-    Serial.print("Kd= ");
-    Serial.print(kd);
-    Serial.println();
-    delay(1000);
-    
-  //}
- // String val=String(Serial.read());
- //Serial.println(val);
-
 }
 
-double calculatePID(double distance, double setpoint, double kp, double ki, double kd) {
-  double PIDValue ;
-  error = distance - setpoint;
-  p_term = kp*error;
-  i_term = i_term + ki*error*dt/1000;
-  d_term = kd*(error-last_error)*1000/dt;;
-  last_error = error;
-  PIDValue = p_term + i_term + d_term;
-  Serial.print("pid = ");
-  Serial.print(PIDValue);
-  Serial.println();
-  return PIDValue ;
- //analogWrite(controlPin, PIDValue);
-}
-
-int calibracion(double distance, double distancia_maxima, double distancia_minima) {
-  int para_actuador ;
-  int minpwm = 0 ;
-  int maxpwm = 255 ;
-  if (distance < distancia_minima) {
-    para_actuador = minpwm ;
-  } else if (distance > distancia_maxima) {
-    para_actuador = maxpwm ;
-  } else {
-    para_actuador = int(minpwm + (maxpwm - minpwm) * PIDValue / (distancia_maxima - distancia_minima)) ; // es perfectible
-  }
-  Serial.println(para_actuador) ;
-  return para_actuador ;
-}
-
-int input_actuador(double PIDv) {
-  int para_actuador ;
-  if (PIDv < 0) {
-    para_actuador = 255 ;
-  } else if (PIDv > 255) {
-    para_actuador = 0 ;
-  } else {
-    para_actuador = 255 - PIDv ;
-  }
-  Serial.println(para_actuador) ;
-  return para_actuador ;
+void showNewNumber() {
+    if (newData == true) {
+        dataNumber = 0;             // new for this version
+        dataNumber = atoi(receivedChars);   // new for this version
+        Serial.print("This just in ... ");
+        Serial.println(receivedChars);
+        Serial.print("Data as Number ... ");    // new for this version
+        Serial.println(dataNumber);     // new for this version
+        newData = false;
+    }
 }
